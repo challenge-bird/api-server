e390859eb46c02936a3a00f617df606f3bd77ece
jparangdev <jparangdev@gmail.com>
2022-02-27 17:10:37 +0900

    setting (travis) : 트래비스 기능 추가 - 개발브런치 반영시 트레비스 스크립트가 실행되는지 확인
    개발 배포기능 추가

--- src/main/java/kr/co/challengebird/apiserver/web/hello/HelloController.java
+++ src/main/java/kr/co/challengebird/apiserver/web/hello/HelloController.java
@@ -5,6 +5,8 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.challengebird.apiserver.web.dto.ResponseDto;
@@ -23,5 +25,15 @@ public class HelloController {
 		String res = "hello bird~! Json..";
 		response.put("data", res);
 		return ResponseEntity.ok(response);
+	}
+
+	@PostMapping("/helloPost")
+	public ResponseEntity<Map<String, Object>> helloJson(@RequestBody Map<String, Object> param) {
+		String test = String.valueOf(param.get("test"));
+
+		test += " bird~!!!";
+
+		param.put("test",test);
+		return ResponseEntity.ok(param);
 	}
 }
