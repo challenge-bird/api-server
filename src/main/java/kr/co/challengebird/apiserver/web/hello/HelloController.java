40b6642e94e39610b57f4336a738a7c0f820a32c
jparangdev <jparangdev@gmail.com>
2022-03-13 21:44:18 +0900

    setting (gitbuh action) : 깃허브액션 테스트 - 도커 컴포즈 빌드 테스트
    깃 허브 액션 테스트

--- src/main/java/kr/co/challengebird/apiserver/web/hello/HelloController.java
+++ src/main/java/kr/co/challengebird/apiserver/web/hello/HelloController.java
@@ -31,7 +31,7 @@ public class HelloController {
 	public ResponseEntity<Map<String, Object>> helloJson(@RequestBody Map<String, Object> param) {
 		String test = String.valueOf(param.get("test"));
 
-		test += " post bird~!!!!a2";
+		test += " post bird~!!!! 자동빌드설정테스트트트";
 
 		param.put("test",test);
 		return ResponseEntity.ok(param);
